ecb625a98f7d023c8f3137ebc63e9d07e44b74fe
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-10 21:34:12 +0300

    Update payments_table.sql
    Added another trigger

--- payments_table.sql
+++ payments_table.sql
@@ -16,3 +16,24 @@ CREATE TRIGGER trigger_add_payment_due_date
 AFTER INSERT ON projects
 FOR EACH ROW
 EXECUTE FUNCTION fn_set_payment_due_date();
+
+--When the status of a project is changed as True upon payment, the below trigger will set the payment_date in the payments table as the "current date" of status change.
+
+CREATE OR REPLACE FUNCTION fn_set_payment_date()
+RETURNS TRIGGER
+AS $$
+    BEGIN
+    IF NEW.status_paid = TRUE THEN
+        UPDATE payments
+        SET date_paid = (SELECT CURRENT_TIMESTAMP)
+        WHERE payments.project_id = NEW.id;
+    END IF;
+    RETURN NEW;
+    END
+ $$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER set_payment_date
+AFTER UPDATE OF status_paid ON projects
+FOR EACH ROW
+EXECUTE FUNCTION fn_set_payment_due_date();
