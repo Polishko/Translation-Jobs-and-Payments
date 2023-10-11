1cf11342aafff751be8672dc6cb4a7331931c7b8
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-11 11:21:17 +0300

    Delete rate_percentages_table.sql
    Deleted this repeating file

--- rate_percentages_table.sql
+++
@@ -1,8 +0,0 @@
--- This table shows the percentage of the main rate paid for No match and fuzzy segments (75-84% match and 85-99% match)
-
-INSERT INTO rate_percentages(match_type, percent_of_full_rate)
-VALUES
-    ('No Match', 100),
-    ('FZ/75-84', 50),
-    ('FZ/85-99', 30)
-;
