2151f5671ecc3889450d277c6a450baa7be5ed82
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-11 16:38:25 +0300

    Update 5. the_projects_table_info.sql
    Added new procedure for easy insertion of new projects to project table

--- 5. the_projects_table_info.sql
+++ 5. the_projects_table_info.sql
@@ -7,6 +7,15 @@ It also contains a column for price that is calculated using the fn_calculate_pr
 INSERT INTO projects(name, start_date, end_date, account_id, job_type_id, low_fuzzy, high_fuzzy, no_match, price)
 VALUES
     ('156_BCP Manual TR', '09.27.2023 10:10', '10.02.2023 09:00', 6, 2, 128, 287, 5243, (SELECT fn_calculate_price_for_project(2, 128, 287, 5243))),
-....
 
-  
+    -- I included a procedure sp_insert_project_into_projects_table which uses this function and simple copyin from the notification email, to add new values to the table. Can be called as shown:
+
+
+CALL sp_insert_project_into_projects_table(
+    'Part 2',                 --copy paste project name
+    '12.10.2023, 15:30',      --copy paste delivery date
+    'Synology',               --copy paste account name
+    'Post Edit',              --copy paste job type
+    0,                        --enter low fuzzy high fuzzy and o match segments respectively 
+    673,
+    2595);
