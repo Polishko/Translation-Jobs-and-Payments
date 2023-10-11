7aa5592cde738d6fbf11326c4d9756914bb2eb62
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-11 10:56:18 +0300

    Update and rename accounts_table.sql to 1. the_accounts_table_info.sql
    Updated table accounts info

---
+++ 1. the_accounts_table_info.sql
@@ -0,0 +1,8 @@
+--This table contains information about the accounts in two columns: Full name of the accounts in one column and the abbreviation of the account name in another. 
+
+--Example: 
+INSERT INTO accounts(name, code )
+VALUES
+    ('Synology', 'SYN'),
+    ...
+;
