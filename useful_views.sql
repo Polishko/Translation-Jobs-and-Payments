f75acf45776a92c4a3ad179710ae61b607d3b193
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-10-11 12:55:42 +0300

    Update useful_views.sql
    changed view

--- useful_views.sql
+++ useful_views.sql
@@ -1,4 +1,6 @@
---1. A view which is used to check whether a certain project that is completed (and not paid) lacks a purchase order. It can be retrieved before the end of the month, prior to preparing the necessary invoices.
+/*1. MISSING POs
+A view which is used to check whether a certain project that is completed (and not paid) lacks a purchase order.
+It can be retrieved before the end of the month, prior to preparing the necessary invoices.*/
 
 DROP VIEW IF EXISTS projects_without_po;
 CREATE VIEW projects_without_po AS(
@@ -10,7 +12,7 @@ CREATE VIEW projects_without_po AS(
                   ON pr.account_id = a.id
              LEFT JOIN purchase_orders AS po
                        ON pr.id = po.project_id
-    WHERE pr.end_date < CURRENT_TIMESTAMP
+    WHERE delivered_status = TRUE
       AND pr.status_paid = FALSE
       AND po.id IS NULL
     ORDER BY pr.end_date
